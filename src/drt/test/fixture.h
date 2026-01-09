f9f54a846f019252ac614cb050938a14e36fabe0
Damon Anderson <damon@h3dgamma.com>
2026-01-09 09:28:07 -0500

    Testbench required updates so that minimumCut numCuts was actually set; May need to include more test cases for robustness
    Signed-off-by: Damon Anderson <damon@h3dgamma.com>

--- src/drt/test/fixture.h
+++ src/drt/test/fixture.h
@@ -167,6 +167,7 @@ class Fixture : public ::testing::Test
       frCoord firstOverhang,
       frCoord secondOverhang);
   void makeMinimumCut(frLayerNum layerNum,
+  					  int numCut,
                       frCoord width,
                       frCoord length,
                       frCoord distance,
